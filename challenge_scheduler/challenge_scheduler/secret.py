b1a32d9dd60978093ea2b1c1349e00a1eb86d216
Pawel Zukowski <p.z.idlecode@gmail.com>
2018-11-13 21:40:20 +0100

    Integrate common pre-commit hooks

---
+++ challenge_scheduler/challenge_scheduler/secret.py
@@ -0,0 +1,3 @@
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY", "gw9!m=i9$^(#yd@qb7$o5h6yzh5d1dvo_wo%)ezf!$n3+6(wb$")
--- challenge_scheduler/website/views.py
+++ challenge_scheduler/website/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+# from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    del request
+    return HttpResponse("Hello there")
