b623501d3cbdf3e3c03ea707ccc6ba06e20f24ac
Pawel Zukowski <p.z.idlecode@gmail.com>
2018-11-16 22:21:05 +0100

    Add crude login page with basic logic

--- challenge_scheduler/website/views.py
+++ challenge_scheduler/website/views.py
@@ -1,14 +1,37 @@
-# from django.http import HttpResponse
-# from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.views.generic import RedirectView, TemplateView
 
-# Create your views here.
-from django.views.generic import TemplateView
 
-
-class HomeView(TemplateView):
+class HomeView(TemplateView, LoginRequiredMixin):
     template_name = "website/home.html"
+    login_url = "/login/"
 
     def get_context_data(self, **kwargs):
         from datetime import datetime
 
         return {"my_date": datetime.now()}
+
+
+class LoginView(TemplateView):
+    template_name = "website/login.html"
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        del args, kwargs
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user: User = authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+        return self.render_to_response({"user": user})
+
+
+class LogoutView(RedirectView):
+    url = "/"
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        print("fLogging out {request.user}")
+        logout(request)
+        return super().get(request, *args, **kwargs)
